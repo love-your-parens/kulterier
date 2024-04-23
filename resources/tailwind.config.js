25e912dc7b8bc34464a1de3f11e00778644beda7
Konrad Wątor <konrad@wator.it>
2024-04-24 01:57:34 +0200

    Add fonts and accents

--- resources/tailwind.config.js
+++ resources/tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
     './src/**/*',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        misty: {
+          500: '#6c5a9d',
+          800: '#433a5c',
+        },
+      },
+      fontFamily: {
+        'sans': ["Switzer", "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
