32f866dc27fc3a10a1430e6f0e9f03462d320eef
Konrad Wątor <konrad@wator.it>
2024-04-24 01:59:01 +0200

    Provide complete GUI/UX for event lists

--- src/kulterier/home.clj
+++ src/kulterier/home.clj
@@ -8,132 +8,42 @@
             [xtdb.api :as xt]
             [kulterier.scraper :as scraper]))
 
-(def email-disabled-notice
-  [:.text-sm.mt-3.bg-blue-100.rounded.p-2
-   "Until you add API keys for MailerSend and reCAPTCHA, we'll print your sign-up "
-   "link to the console. See config.edn."])
+(defn permanent-events [_]
+  (ui/event-tab-container :permanent
+                          (ui/permanent-events-table
+                           (:permanent (scraper/get-events)))))
 
-(defn home-page [{:keys [recaptcha/site-key params] :as ctx}]
-  (ui/page
-   ctx
-   [:header.text-center.mb-6
-    [:div.inline-block
-     [:h1 {:class ["text-5xl font-black"]} "Kulterier"]
-     [:h2.text-2xl.font-medium "Na tropie kultury"]]]
-   (ui/timetable-events-table (:timetable (scraper/get-events)))))
+(defn temporary-events [_]
+  (ui/event-tab-container :temporary (ui/temporary-events-table
+                                      (:temporary (scraper/get-events)))))
 
-(defn link-sent [{:keys [params] :as ctx}]
-  (ui/page
-   ctx
-   [:h2.text-xl.font-bold "Check your inbox"]
-   [:p "We've sent a sign-in link to " [:span.font-bold (:email params)] "."]))
+(defn timetable-events [_]
+  (ui/event-tab-container :timetable
+                   (ui/timetable-events-table
+                    (:timetable (scraper/get-events)))))
 
-(defn verify-email-page [{:keys [params] :as ctx}]
-  (ui/page
-   ctx
-   [:h2.text-2xl.font-bold (str "Sign up for " settings/app-name)]
-   [:.h-3]
-   (biff/form
-    {:action "/auth/verify-link"
-     :hidden {:token (:token params)}}
-    [:div [:label {:for "email"}
-           "It looks like you opened this link on a different device or browser than the one "
-           "you signed up on. For verification, please enter the email you signed up with:"]]
-    [:.h-3]
-    [:.flex
-     [:input#email {:name "email" :type "email"
-                    :placeholder "Enter your email address"}]
-     [:.w-3]
-     [:button.btn {:type "submit"}
-      "Sign in"]])
-   (when-some [error (:error params)]
-     [:.h-1]
-     [:.text-sm.text-red-600
-      (case error
-        "incorrect-email" "Incorrect email address. Try again."
-        "There was an error.")])))
-
-(defn signin-page [{:keys [recaptcha/site-key params] :as ctx}]
-  (ui/page
-   (assoc ctx ::ui/recaptcha true)
-   (biff/form
-    {:action "/auth/send-code"
-     :id "signin"
-     :hidden {:on-error "/signin"}}
-    (biff/recaptcha-callback "submitSignin" "signin")
-    [:h2.text-2xl.font-bold "Sign in to " settings/app-name]
-    [:.h-3]
-    [:.flex
-     [:input#email {:name "email"
-                    :type "email"
-                    :autocomplete "email"
-                    :placeholder "Enter your email address"}]
-     [:.w-3]
-     [:button.btn.g-recaptcha
-      (merge (when site-key
-               {:data-sitekey site-key
-                :data-callback "submitSignin"})
-             {:type "submit"})
-      "Sign in"]]
-    (when-some [error (:error params)]
-      [:<>
-       [:.h-1]
-       [:.text-sm.text-red-600
-        (case error
-          "recaptcha" (str "You failed the recaptcha test. Try again, "
-                           "and make sure you aren't blocking scripts from Google.")
-          "invalid-email" "Invalid email. Try again with a different address."
-          "send-failed" (str "We weren't able to send an email to that address. "
-                             "If the problem persists, try another address.")
-          "invalid-link" "Invalid or expired link. Sign in to get a new link."
-          "not-signed-in" "You must be signed in to view that page."
-          "There was an error.")]])
-    [:.h-1]
-    [:.text-sm "Don't have an account yet? " [:a.link {:href "/"} "Sign up"] "."]
-    [:.h-3]
-    biff/recaptcha-disclosure
-    email-disabled-notice)))
-
-(defn enter-code-page [{:keys [recaptcha/site-key params] :as ctx}]
-  (ui/page
-   (assoc ctx ::ui/recaptcha true)
-   (biff/form
-    {:action "/auth/verify-code"
-     :id "code-form"
-     :hidden {:email (:email params)}}
-    (biff/recaptcha-callback "submitCode" "code-form")
-    [:div [:label {:for "code"} "Enter the 6-digit code that we sent to "
-           [:span.font-bold (:email params)]]]
-    [:.h-1]
-    [:.flex
-     [:input#code {:name "code" :type "text"}]
-     [:.w-3]
-     [:button.btn.g-recaptcha
-      (merge (when site-key
-               {:data-sitekey site-key
-                :data-callback "submitCode"})
-             {:type "submit"})
-      "Sign in"]])
-   (when-some [error (:error params)]
-     [:.h-1]
-     [:.text-sm.text-red-600
-      (case error
-        "invalid-code" "Invalid code."
-        "There was an error.")])
-   [:.h-3]
-   (biff/form
-    {:action "/auth/send-code"
-     :id "signin"
-     :hidden {:email (:email params)
-              :on-error "/signin"}}
-    (biff/recaptcha-callback "submitSignin" "signin")
-    [:button.link.g-recaptcha
-     (merge (when site-key
-              {:data-sitekey site-key
-               :data-callback "submitSignin"})
-            {:type "submit"})
-     "Send another code"])))
+(defn home-page [content-uri]
+  (fn [{:keys [recaptcha/site-key params] :as ctx}]
+    (ui/page
+     ctx
+     [:header.text-center.mb-6
+      [:div.inline-block
+       [:h1 {:class ["text-5xl font-black"]} "Kulterier"]
+       [:h2.text-2xl.font-light.lowercase "Na tropie kultury"]]]
+     [:section#content {:hx-trigger "load delay:100ms"
+                       :hx-get content-uri
+                       :hx-target "this"
+                       :hx-swap "innerHTML"}
+      [:p.text-center "Węszę..."]])))
 
 (def module
   {:routes [["" {:middleware [mid/wrap-redirect-signed-in]}
-             ["/"                  {:get home-page}]]]})
+             ["/" {:get (home-page "/events/timetable")}]
+             ["/permanent" {:get (home-page "/events/permanent")}]
+             ["/temporary" {:get (home-page "/events/temporary")}]
+             ["/timetable" {:get (home-page "/events/timetable")}]
+]
+            ["/events"
+             ["/permanent" {:get permanent-events}]
+             ["/temporary" {:get temporary-events}]
+             ["/timetable" {:get timetable-events}]]]})
--- src/kulterier/ui.clj
+++ src/kulterier/ui.clj
@@ -28,12 +28,13 @@
    biff/base-html
    (-> ctx
        (merge #:base{:title settings/app-name
-                     :lang "en-US"
+                     :lang "pl-PL"
                      :icon "/img/glider.png"
                      :description (str settings/app-name " Description")
                      :image "https://clojure.org/images/clojure-logo-120b.png"})
        (update :base/head (fn [head]
                             (concat [[:link {:rel "stylesheet" :href (css-path)}]
+                                     [:link {:rel "stylesheet" :href "/css/switzer.css"}]
                                      [:script {:src (js-path)}]
                                      [:script {:src "https://unpkg.com/htmx.org@1.9.10"}]
                                      [:script {:src "https://unpkg.com/htmx.org/dist/ext/ws.js"}]
@@ -48,7 +49,7 @@
   (base
    ctx
    [:.flex-grow]
-   [:.mx-auto.w-full.md:p-6
+   [:.mx-auto.w-full.py-2.md:p-6
     (when (bound? #'csrf/*anti-forgery-token*)
       {:hx-headers (cheshire/generate-string
                     {:x-csrf-token csrf/*anti-forgery-token*})})
@@ -67,31 +68,6 @@
               "Page not found."
               "Something went wrong.")]))})
 
-(defn generic-events-table
-  [events]
-  (let [e events]
-    [:table
-     [:tbody
-      (for [[i d] e]
-        (let [top-row [[:td [:a {:href (:url d)}
-                             [:img {:src (:thumbnail d)
-                                    :class ["min-w-[80px]" "w-[130px]" "max-h-[130px]"
-                                            "object-cover" "object-top"]}]]]
-                       [:td.p-2 [:a {:href (:url d)} (:name d)]]
-                       [:td.p-2 (:place d)]
-                       [:td.p-2 (:type d)]
-                       [:td.p-2 (:date d)]
-                       [:td.p-2 (:date-type d)]]]
-          [:<>
-           (into [:tr {:class "event-breakdown"
-                       :data-event-id i}] top-row)
-           [:tr {:class "event-description"
-                 :data-event-id i}
-            [:td {:colspan (count top-row)
-                  :class [".max-h-1"]}
-             (:description d)]]]))]
-     [:tfoot]]))
-
 (defn text-link
   ([url] (text-link url url))
   ([text url]
@@ -109,7 +85,7 @@
                  "whitespace-nowrap"
                  "text-ellipsis"
                  "overflow-hidden"
-                 "max-w-[50vw]"]}
+                 "max-w-[40vw]"]}
     (when thumbnail
       [:a {:href url}
        [:img {:src thumbnail
@@ -119,11 +95,25 @@
                       "border-l-2" "pl-0.5" "border-black"]}]])
     (if url (text-link title url) title)]))
 
+(defn event-type-tag
+  [event-type-key]
+  [:span.font-light.text-slate-600
+   (str "#" (case event-type-key
+              :museum "muzeum"
+              :movie "kino"
+              "inne"))])
+
+(comment
+  (event-type-tag :whatever)
+  (event-type-tag :movie)
+  )
+
 (defn title-row
   [colspan title url thumbnail]
   [:tr
    [:td {:colspan colspan
          :class ["md:hidden"
+                 "max-w-[90vw]"
                  "font-bold"
                  "text-lg"
                  "whitespace-nowrap"
@@ -134,36 +124,129 @@
        [:img {:src thumbnail
               :alt title
               :class ["inline" "min-w-[80px]" "w-[90px]" "max-h-[90px]"
-                      "mr-2" "md:mr-4" "object-cover" "object-top"
+                      "mr-3" "md:mr-4" "object-cover" "object-top"
                       "border-l-2" "pl-0.5" "border-black"]}]])
     (if url (text-link title url) title)]])
 
+(defn description-row
+  [colspan event-description event-id]
+  [:tr {:class ["event-description"]
+        :data-event-id event-id}
+   (let [description (str event-description)
+         not-blank? (not (.isBlank description))]
+     [:td {:colspan colspan :class ["md:px-2.5" "pb-3" "md:pb-0"]}
+      (when not-blank?
+        [:<>
+         [:div.float-left.text-xl.font-mono
+          {:class "-mt-1"}
+          "↳"]
+         [:p.ml-4.mr-1.text-justify
+          description]])])])
+
 (defn timetable-events-table
   [events]
-  (let [e (sort-by (comp :date second) events)]
-    [:table {:class ["m-auto" "max-w-[95%]" "md:max-w-[75%]"]}
+  (let [colspan (-> events first second count)
+        grouped (group-by (fn [[_ event]]
+                            (.format (:date event)
+                                     (java.time.format.DateTimeFormatter/ofPattern "Y/MM/dd")))
+                          (sort-by (comp :date second) events))]
+    [:table {:class ["m-auto" "-mt-2" "max-w-[95%]" "md:max-w-[1024px]"]}
      [:tbody
-      (for [[i d] e]
-        (let [summary-row [(title-cell (:name d) (:url d) (:thumbnail d))
-                       [:td.text-left.md:p-2 (:place d)]
-                       [:td.text-center.md:text-left.md:p-2 (:type d)]
-                       [:td.text-right.md:p-2 (when-let [d ^java.time.ZonedDateTime (:date d)]
-                                     (.format d (java.time.format.DateTimeFormatter/ofPattern
-                                                 "Y/MM/dd HH:mm")))]]]
-          [:<>
-           (into [:tr {:class "event-breakdown"
-                       :data-event-id i}] summary-row)
-           (title-row (count summary-row) (:name d) (:url d) (:thumbnail d))
-           [:tr {:class "event-description"
-                 :data-event-id i}
-            (let [description (str (:description d))]
-              (when-not (.isBlank description)
-                [:td.px-2.pb-3 {:colspan (count summary-row)}
-                 [:div.float-left.text-xl.-mt-1.font-mono "↳"]
-                 [:p.ml-4.mr-1 description]]))]]))]
+      (for [[g e] grouped]
+        (into [:<>
+               [:tr [:td {:class ["text-xl" "font-light" "text-left" "pt-1"]
+                          :colspan colspan}
+                     [:p {:class ["border-b border-dashed border-neutral-400 py-2"]}
+                      (format "» %s" g)]]]]
+              (for [[i d] e]
+                (let [summary-row [(title-cell (:name d) (:url d) (:thumbnail d))
+                                   [:td.text-left.md:p-2 (:place d)]
+                                   [:td.md:text-center.md:p-2.font-semibold
+                                    (when-let [d ^java.time.ZonedDateTime (:date d)]
+                                      (.format d (java.time.format.DateTimeFormatter/ofPattern
+                                                  "Y/MM/dd HH:mm")))]
+                                   [:td.text-right.md:p-2 (event-type-tag (:type d))]]
+                      colspan (count summary-row)]
+                  [:<>
+                   (into [:tr {:class "event-breakdown"
+                               :data-event-id i}]
+                         summary-row)
+                   (title-row colspan (:name d) (:url d) (:thumbnail d))
+                   (description-row colspan (:description d) i)]))))]
      [:tfoot]]))
 
-(comment
-  (let [d ^java.time.ZonedDateTime (java.time.ZonedDateTime/now)]
-    (.format d (java.time.format.DateTimeFormatter/ofPattern "Y/MM/dd H:m")))
-  )
+(defn permanent-events-table
+  [events]
+  [:table {:class ["mx-auto" "my-4" "max-w-[95%]" "md:max-w-[1024px]"]}
+   [:tbody
+    (for [[i d] events]
+      (let [summary-row [(title-cell (:name d) (:url d) (:thumbnail d))
+                         [:td.text-left.md:p-2 (:place d)]
+                         [:td.text-right.md:p-2 (event-type-tag (:type d))]]
+            colspan (count summary-row)]
+        [:<>
+         (into [:tr {:class "event-breakdown"
+                     :data-event-id i}]
+               summary-row)
+         (title-row colspan (:name d) (:url d) (:thumbnail d))
+         (description-row colspan (:description d) i)]))]
+   [:tfoot]])
+
+
+(defn temporary-events-table
+  [events]
+  (let [sorted-events (sort-by (comp second :date second) events)]
+    [:table {:class ["mx-auto" "my-4" "max-w-[95%]" "md:max-w-[1024px]"]}
+     [:tbody
+      (for [[i d] sorted-events]
+        (let [summary-row [(title-cell (:name d) (:url d) (:thumbnail d))
+                           [:td.text-left.md:p-2 (:place d)]
+                           [:td.text-center.md:text-center.px-1.md:p-2.font-semibold
+                            (-> d :date first)]
+                           [:td.text-center.md:text-center.px-1.md:p-2.font-semibold
+                            (-> d :date second)]
+                           [:td.text-right.md:p-2 (event-type-tag (:type d))]]
+              colspan (count summary-row)]
+          [:<>
+           (into [:tr {:class "event-breakdown"
+                       :data-event-id i}]
+                 summary-row)
+           (title-row colspan (:name d) (:url d) (:thumbnail d))
+           (description-row colspan (:description d) i)]))]
+     [:tfoot]]))
+
+(defn event-tab
+  [key label selected?]
+  (let [keyname (name key)]
+    [:label.m-2 {:class ["font-bold" "cursor-pointer" "transition-all" "ease-in-out" "underline-offset-4"
+                         "hover:scale-110" "hover:underline" "hover:mx-3" "has-[:checked]:underline"]}
+     [:input {:class ["mx-1" "text-slate-700" "focus:ring-slate-600" "border-gray-400"]
+              :type "radio" :name "category"
+              :role "tab" :aria-controls "event-panel"
+              :hx-get (str "/events/" keyname)
+              :hx-push-url (str "/" keyname)
+              :value keyname
+              :aria-selected (str selected?)
+              :checked (when selected? "1")}]
+     label]))
+
+(defn event-tab-list
+  [selected-tab]
+  [:<>
+   [:fieldset {:role "tablist" :class ["flex" "justify-center" "items-center" "my-4"]}
+    (event-tab :permanent "Stałe" (= selected-tab :permanent))
+    [:div "•"]
+    (event-tab :temporary "Tymczasowe" (= selected-tab :temporary))
+    [:div "•"]
+    (event-tab :timetable "Pojedyncze" (= selected-tab :timetable))]])
+
+(defn event-tab-panel
+  [content]
+  [:div {:aria-role "tabpanel"}
+   content])
+
+(defn event-tab-container
+  [selected-tab tab-content]
+  [:<>
+   (event-tab-list selected-tab)
+   (event-tab-panel tab-content)])
