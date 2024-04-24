f4881f024f9f5912a31a4b4eeeb1f16207700afb
Konrad Wątor <konrad@wator.it>
2024-04-24 20:42:43 +0200

    Fix formatting

--- src/kulterier/home.clj
+++ src/kulterier/home.clj
@@ -19,21 +19,27 @@
 
 (defn timetable-events [_]
   (ui/event-tab-container :timetable
-                   (ui/timetable-events-table
-                    (:timetable (scraper/get-events)))))
+                          (ui/timetable-events-table
+                           (:timetable (scraper/get-events)))))
 
 (defn home-page [content-uri]
   (fn [{:keys [recaptcha/site-key params] :as ctx}]
     (ui/page
      ctx
      [:header.text-center.mb-6
+      (let [[tag params & rest] (ui/kulterier-logo)]
+        (into [tag (merge params
+                          {:width 160
+                           :height 160
+                           :class ["m-auto" "text-center"]})]
+              rest))
       [:div.inline-block
        [:h1 {:class ["text-5xl font-black"]} "Kulterier"]
        [:h2.text-2xl.font-light.lowercase "Na tropie kultury"]]]
      [:section#content {:hx-trigger "load delay:100ms"
-                       :hx-get content-uri
-                       :hx-target "this"
-                       :hx-swap "innerHTML"}
+                        :hx-get content-uri
+                        :hx-target "this"
+                        :hx-swap "innerHTML"}
       [:p.text-center "Węszę..."]])))
 
 (def module
@@ -41,8 +47,7 @@
              ["/" {:get (home-page "/events/timetable")}]
              ["/permanent" {:get (home-page "/events/permanent")}]
              ["/temporary" {:get (home-page "/events/temporary")}]
-             ["/timetable" {:get (home-page "/events/timetable")}]
-]
+             ["/timetable" {:get (home-page "/events/timetable")}]]
             ["/events"
              ["/permanent" {:get permanent-events}]
              ["/temporary" {:get temporary-events}]
