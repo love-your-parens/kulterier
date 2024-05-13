c1b481d22014d110e2e3fd6efd5a030901a4a4a4
Konrad Wątor <konrad@wator.it>
2024-05-14 00:28:16 +0200

    Expand UI/UX
    - SVG UI elements relegated to separate ns
    - added scroll to top element
    - added filtering UI

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,1 +1,28 @@
 // When plain htmx isn't quite enough, you can stick some custom JS here.
+document.addEventListener(
+    "DOMContentLoaded",
+    function () {
+        const backToTop = document.querySelector("#back-to-top")
+        backToTop.addEventListener('click', function () {
+            window.scroll({ top: 0, behavior: 'smooth' })
+        })
+
+        let scrollHookRunning = false;
+        document.addEventListener("scroll", () => {
+            if (!scrollHookRunning) {
+                running = true;
+                window.requestAnimationFrame(() => {
+                    if (window.scrollY > window.innerHeight) {
+                        backToTop.classList.remove("pointer-events-none")
+                        backToTop.classList.remove("opacity-0")
+
+                    } else {
+                        backToTop.classList.add("pointer-events-none")
+                        backToTop.classList.add("opacity-0")
+                    }
+                    running = false;
+                })
+            }
+        })
+    }
+)
