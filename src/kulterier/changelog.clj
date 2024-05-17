c0294efb9187e4a7d96985707617b8ac3c3a207c
Konrad Wątor <konrad@wator.it>
2024-05-17 21:31:06 +0200

    Redo the automatic changelog view generator

--- src/kulterier/changelog.clj
+++ src/kulterier/changelog.clj
@@ -2,22 +2,23 @@
   (:require [com.biffweb :as biff]
             [kulterier.ui :as ui]
             [kulterier.settings :as settings]
-            [markdown-to-hiccup.core :as mth]))
+            [markdown.core :as md]))
+
 
 ;; Note: only read/process the changelog once per app run.
 ;; Extremely unlikely for the changelog to shift without an app restart.
 (def *changelog
-  (delay (let [[_ _ [_ _ h1] & rest] (-> "CHANGELOG.md"
-                                         mth/file->hiccup
-                                         mth/component)]
-           (cons [:h1.text-2xl.font-black.my-4 h1]
-                 rest))))
+  (delay (biff/unsafe
+          (md/md-to-html-string
+           (slurp "CHANGELOG.md")))))
+
 
 (defn changelog-page
-  [& opts]
-  (ui/page
-   {:base/title (str "Changelog - " settings/app-name)}
-   [:section#changelog @*changelog]))
+    [& opts]
+    (ui/page
+     {:base/title (str "Changelog - " settings/app-name)}
+     [:section#changelog @*changelog]))
+
 
 (def module
   {:routes ["/changelog" {:get changelog-page}]})
